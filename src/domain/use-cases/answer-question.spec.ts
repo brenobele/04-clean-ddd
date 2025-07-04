3f1e8f027a2aaaba1ef45d766660274dc0c8ff45
Breno S. Beletato <breno.beletato@suprema.group>
2025-07-03 22:52:20 -0300

    Aulas: Mapeando relacionamentos / Dependêcias externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,14 +1,22 @@
-import { expect, test } from "vitest"
-import { AnswerQuestionUseCase } from "./answer-question"
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test('Create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer): Promise<void> => {
+    return;
+  },
+};
 
-    const answer = answerQuestion.execute({
-        instructorId: '1',
-        questionId: '1',
-        content: 'Nova resposta'
-    })
+test("Create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
 
-    expect(answer.content).toEqual('Nova resposta')
-})
+  const answer = await answerQuestion.execute({
+    instructorId: "1",
+    questionId: "1",
+    content: "Nova resposta",
+  });
+
+  expect(answer.content).toEqual("Nova resposta");
+});
