b7970c87e59740572d76a4fcfebb962767b67384
Breno S. Beletato <breno.beletato@suprema.group>
2025-08-07 23:22:09 -0300

    Caso de uso: Listar comentários da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,11 +1,16 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
-export class InMemoryQuestionCommentsRepository implements QuestionCommentRepository {
+export class InMemoryQuestionCommentsRepository
+implements QuestionCommentRepository
+{
   public items: QuestionComment[] = []
 
   async findById(id: string) {
-    const questionComment = this.items.find((item) => item.id.toString() === id)
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id,
+    )
 
     if (!questionComment) {
       return null
@@ -14,12 +19,22 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentReposi
     return questionComment
   }
 
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
   }
 
   async delete(questionComment: QuestionComment) {
-    const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
 
     this.items.splice(itemIndex, 1)
   }
